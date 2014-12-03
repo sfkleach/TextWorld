90b7f5d301801293da81756a6e9b5e87aaafb88f
Stephen Leach <sfkleach@gmail.com>
2014-12-03 09:36:38 +0000

    Removed relic code.

--- src/com/steelypip/textworld/gameclasses/Agent.java
+++ src/com/steelypip/textworld/gameclasses/Agent.java
@@ -3,9 +3,6 @@ package com.steelypip.textworld.gameclasses;
 import com.steelypip.powerups.minxml.MinXML;
 
 public class Agent extends Thing {
-
-	public void fixDefaultLocation() {
-	}
 	
 	@Override
 	public void init( MinXML initial_configuration ) {
